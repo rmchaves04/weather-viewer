57bbf2b48696e60c9e827d5ea40536fa8ffef486
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2024-03-04 10:27:48 -0300

    [refactor] Refactor map initialization

--- public/js/map.js
+++ public/js/map.js
@@ -1,29 +1,20 @@
-async function loadMap() {
-    const response = await fetch('/api/maps-key');
-    const apiKey = await response.json();
+'use strict';
 
-    const mapLoader = new Loader({
-        apiKey: apiKey,
-        version: 'weekly',
-        libraries: ['places']
+let map;
+
+async function initMap() {
+
+    const response = await fetch('/api/maps-key');
+    const key = await response.json();
+
+    (g=>{var h,a,k,p='The Google Maps JavaScript API',c='google',l='importLibrary',q='__ib__',m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement('script'));e.set('libraries',[...r]+'');for(k in g)e.set(k.replace(/[A-Z]/g,t=>'_'+t[0].toLowerCase()),g[k]);e.set('callback',c+'.maps.'+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+' could not load.'));a.nonce=m.querySelector('script[nonce]')?.nonce||'';m.head.append(a)}));d[l]?console.warn(p+' only loads once. Ignoring:',g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+    ({key: key, v: 'weekly'});
+
+    const { Map } = await google.maps.importLibrary('maps');
+
+    map = new Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
     });
     
-    const mapOptions = {
-        center: {
-            lat: -34.397, 
-            lng: 150.644 
-        },
-        zoom: 8
-    };
-    
-    mapLoader
-        .importLibrary('maps')
-        .then(({Map}) => {
-            new Map(document.getElementById('map'), mapOptions);
-        })
-        .catch((e) => {
-            console.error(e);
-        });
 }
-
-loadMap();
