9270af32ebba27f64b1856daba413ab99cad42aa
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2024-03-04 11:04:51 -0300

    [feat] Create location register form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-<script src="{{asset('js/map.js')}}"></script>
+<script src="{{asset('js/map.js')}}" type="module"></script>
 <link rel="stylesheet" href="{{asset('css/map.css')}}">
 @section('content')
 
@@ -7,6 +7,32 @@
         <h1>Weather Viewer</h1>
 
         <hr>
+
+        <form action="#">
+            <div class="row mb-2">
+                <div class="col-auto">
+                    <label for="lat" class="col-form-label">lat</label>
+                </div>
+                <div class="col-auto">
+                    <input type="text" id="lat" class="form-control" readonly>
+                </div>
+                <div class="col-auto">
+                    <label for="lng" class="col-form-label">lng</label>
+                </div>
+                <div class="col-auto">
+                    <input type="text" id="lng" class="form-control" readonly>
+                </div>
+                <div class="col-auto">
+                    <label for="lng" class="col-form-label">location</label>
+                </div>
+                <div class="col-auto">
+                    <input type="text" id="location" class="form-control" readonly>
+                </div>
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-outline-success"></i>Register Location</button>
+                </div>
+            </div>
+        </form>
 
         <div id="map"></div>
     </div>
