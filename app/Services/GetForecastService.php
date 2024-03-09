4d4bac73e3bb9ee703efaf1c5648a6b216f77196
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2024-03-09 13:19:51 -0300

    [feat] Create GetForecastService

---
+++ app/Services/GetForecastService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Location;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Http;
+
+class GetForecastService
+{
+
+    public function getAllForecasts(): Collection {
+        $latitudes = Location::all()->pluck('latitude')->toArray();
+        $longitudes = Location::all()->pluck('longitude')->toArray();
+
+        $response = Http::get('https://api.open-meteo.com/v1/forecast', [
+            'latitude' => $latitudes,
+            'longitude' => $longitudes,
+            'current' => ['temperature_2m_min', 'temperature_2m_max', 'precipitation_probability_mean'],
+            'timezone' => 'auto',
+
+        ])->json();
+
+        dd($response);
+
+        return $this->formatResponse($response);
+    }
+
+    private function formatResponse($response): Collection {
+        $locations = Location::all();
+
+        $i = 0;
+        foreach ($response as $location_forecast) {
+            $locations[$i]['forecast'] = collect([
+                'temperature_min' => $location_forecast['daily']['temperature_2m_min'],
+                'temperature_max' => $location_forecast['daily']['temperature_2m_max'],
+                'precipitation_probability' => $location_forecast['daily']['precipitation_probability_mean']
+            ]);
+            $i++;
+        }
+
+        return $locations;
+    }
+
+}
